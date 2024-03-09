8e6a21eeb8892968fc406bdee8908fe56710b11d
Jesus de Freitas <j5rojo94@gmail.com>
2024-03-09 20:26:17 +0000

    Add Breeze to manage api auth routes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Superadmin',
+             'email' => 'j5rojo94@gmail.com',
+             "password" => bcrypt('password'),
+             "remember_token" => Str::random(10),
+         ]);
     }
 }
